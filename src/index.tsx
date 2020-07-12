719b6e8ef51588f948d9cd2d3cd63cf984975763
Kayo Timoteo Rossales <timoteokayo@gmail.com>
2020-07-11 21:55:46 -0400

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+// import { Container } from './styles';
+
+const src: React.FC = () => {
+  return <Text>Teste</Text>
+}
+
+export default src
